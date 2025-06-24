664f5da1339fce7814564438be21a5964eaebb06
Cristian Primero <soyskiper@gmail.com>
2025-06-24 14:08:49 -0600

    Object stdClass to array and array to stdClass (object)

--- opp/stdClass.php
+++ opp/stdClass.php
@@ -5,5 +5,19 @@ $beer = new stdClass();
 $beer->name = "Heineken";
 $beer->alcohol = 5.0;
 
-$beer->name = "Corona";
-echo $beer->name;
+//necho $beer->name;
+
+$arr = (array) $beer;
+
+// echo $arr['name'];
+
+$arrLocation = [
+    'city' => 'Amsterdam',
+    'country' => 'Netherlands'
+];
+
+$objLocation = (object) $arrLocation;
+
+echo $objLocation->city . ", " . $objLocation->country;
+
+print_r($objLocation);
