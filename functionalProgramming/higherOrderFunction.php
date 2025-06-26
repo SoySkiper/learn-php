f22a52adbe73487258822d0224b7a5640d67a34b
Cristian Primero <soyskiper@gmail.com>
2025-06-26 11:23:09 -0600

    Capturing External Variables with 'use'

--- functionalProgramming/higherOrderFunction.php
+++ functionalProgramming/higherOrderFunction.php
@@ -1,7 +1,9 @@
 <?php
 
-$some = function (float $a, float $b): float {
-    return $a + $b;
+$const = 5;
+
+$some = function (float $a, float $b) use ($const): float {
+    return $a + $b + $const;
 };
 
 $sum = fn (float $a, float $b): float => $a + $b;
@@ -23,4 +25,6 @@ echo "<br>";
 show($sum, 2, 6); // Output: 8
 echo "<br>";
 // Using anonymous function
-show(fn (float $a, float $b): float => $a + $b, 2, 6); // Output: 8
+// show(fn (float $a, float $b): float => $a + $b, 2, 6); // Output: 8
+
+show($some, 2, 6); // Output: 13
